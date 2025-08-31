6f1e6d56c64437b43e7742a460d39ebfbf24f208
Karolis Blazevicius <broliukas@gmail.com>
2025-08-31 19:51:34 +0300

    Moves_NonShelfOrderToNonShelfStorage_WhenShelfOrderArrives.

--- src/Challenge/Main.cs
+++ src/Challenge/Main.cs
@@ -126,6 +126,7 @@ public class Simulation : IDisposable
 
     public async Task<List<Action>> Simulate(Config config, List<Order> orders, CancellationToken ct)
     {
+        // can up this number once PlaceOrders is made reentrant.
         var placeOrderParallelism = 1;
         var orderPlacementTasks = new int[placeOrderParallelism].Select(x => PlaceOrders(config, orders, ct)).ToArray();
         var orderPickupTaskStream = orderPlacementTasks.Merge().WithCancellation(ct);
@@ -182,8 +183,8 @@ public class Simulation : IDisposable
             else
             {
                 Action item = new(order.PickupTime, order.Id, ActionType.Discard, orderActions.Last().Target);
-                Console.WriteLine($"Discarding order {item}");
                 orderActions.Add(item);
+                Console.WriteLine($"Discarding order {item}");
             }
         }
     }
@@ -193,6 +194,10 @@ public class Simulation : IDisposable
         foreach (var order in orders)
         {
             var localNow = _time.GetLocalNow().DateTime;
+            if (order.Id == "o2")
+            {
+                Console.WriteLine($"AAAAAAAAAAAa Starting pickup {new Action(localNow, order.Id, ActionType.Pickup, order.Temp)}");
+            }
             var target = ToTarget(order.Temp);
 
             var pickupInMicroseconds = PickableOrder.RandomBetween(config.min, config.max);
@@ -201,30 +206,91 @@ public class Simulation : IDisposable
 
             using (var placeSemaphore = await GetOrCreateWaitingRepoSemaphore(pickableOrder, ct))
             {
+                // Console.WriteLine($"Starting pickup2 {new Action(localNow, order.Id, ActionType.Pickup, order.Temp)}");
                 _pickableOrders.Add(pickableOrder);
 
                 var actions = _actionRepo.Values.Select(x => x.Actions).SelectMany(x => x).ToList();
                 if (IsShelf(target))
                 {
+                    if (IsFull_Flawed(target, config.storageLimits, _actionRepo))
+                    {
+                        // move / discard
+                        var kvpsWithOrdersOnTarget = KvpsWithOrdersOnTarget(_actionRepo, target);
+                        if (!kvpsWithOrdersOnTarget.Any())
+                        {
+                            // handle error from race condition
+                            throw new Exception("the list is migh be empty bcs of race condition");
+                        }
+                        var entriesForOrdersToMove = EntriesForForeignOrdersOnTarget(kvpsWithOrdersOnTarget);
+                        PickableOrder? orderToMove = null;
+
+                        foreach (var entryForOrderToMove in entriesForOrdersToMove)
+                        {
+
+                            //         // try move from shelf
+                            var targetToMoveTo = ToTarget(entryForOrderToMove.Order.Temp);
+                            Console.WriteLine("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa " + entryForOrderToMove.Order.Id);
+                            if (!IsFull_Flawed(targetToMoveTo, config.storageLimits, _actionRepo))
+                            {
+                                orderToMove = entryForOrderToMove.Order;
+                                break;
+                            }
+
+                        } //end for
+
+                        if (orderToMove is not null)
+                        {
+                            using var moveSemaphore = await GetOrCreateWaitingRepoSemaphore(orderToMove, ct);
+                            // check if can move first
+                            // if can not move handle error from race condition.
+                            Action moveAction = new(_time.GetLocalNow().DateTime, orderToMove.Id, ActionType.Move, ToTarget(orderToMove.Temp));
+                            _actionRepo[orderToMove.Id].Actions.Add(
+                                              moveAction);
+                            Console.WriteLine($"Moving order: {moveAction}");
+                        }
+                        else
+                        {
+                            var kvpToDiscard = CalculateOrderToDiscard(kvpsWithOrdersOnTarget);
+                            // need to discard
+                            var orderToDiscard = kvpToDiscard.Value.Order;
+                            using var discardSemaphore = await GetOrCreateWaitingRepoSemaphore(orderToDiscard, ct);
+                            // check if can discard
+                            // if can not discard ... assume moved or processed .. therefore do nothing.
+                            // _actionRepo[orderToDiscard.Id].Actions.Add(
+                            //     new Action(_time.GetLocalNow().DateTime, orderToDiscard.Id, ActionType.Discard, kvpToDiscard.Value.Actions.Last().Target));
+                            // Console.WriteLine($"Placing order: {action}");
+                        }
+
+
+                    }
+
+                    // todo kb: should exit placement and reenter instead.
+                    // place after move/discard
                     _actionRepo[order.Id] = (pickableOrder, new() { });
-                    _actionRepo[order.Id].Actions.Add(new(localNow, order.Id, ActionType.Place, target));
-                    Console.WriteLine($"Order placed: {order}");
+                    Action action = new(localNow, order.Id, ActionType.Place, target);
+                    _actionRepo[order.Id].Actions.Add(action);
+                    Console.WriteLine($"Placing order: {action}");
                 }
                 else
                 {
-                    if (IsFull(target, config.storageLimits, actions))
+                    if (IsFull_Flawed(target, config.storageLimits, _actionRepo))
                     {
+                        Console.WriteLine("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa " + entryForOrderToMove.Order.Id);
+
                         // add action repo entry
                         _actionRepo[order.Id] = (pickableOrder, new() { });
                         // update action repo entry
-                        _actionRepo[order.Id].Actions.Add(new(localNow, order.Id, ActionType.Place, Target.Shelf));
-                        Console.WriteLine($"Order placed: {order}");
+                        Action action = new(localNow, order.Id, ActionType.Place, Target.Shelf);
+                        _actionRepo[order.Id].Actions.Add(action);
+                        Console.WriteLine($"Placing order: {action}");
                     }
                     else
                     {
                         _actionRepo[order.Id] = (pickableOrder, new() { });
-                        _actionRepo[order.Id].Actions.Add(new(localNow, order.Id, ActionType.Place, target));
-                        Console.WriteLine($"Order placed: {order}");
+                        Action action = new(localNow, order.Id, ActionType.Place, target);
+                        _actionRepo[order.Id].Actions.Add(action);
+                        Console.WriteLine($"Placing order: {action}");
+
                     }
                 }
 
@@ -239,6 +305,13 @@ public class Simulation : IDisposable
 
             await WaitUntil(localNow.AddMicroseconds(config.rate), localNow, ct);
         }
+    }
+
+    private KeyValuePair<string, (PickableOrder Order, List<Action> Actions)> CalculateOrderToDiscard(
+        List<KeyValuePair<string, (PickableOrder Order, List<Action> Actions)>> kvpsWithOrdersOnTarget)
+    {
+        // temp implementation
+        return kvpsWithOrdersOnTarget.First();
     }
 
     private async Task WaitUntil(DateTime targetTime, DateTime baseLocalNow, CancellationToken ct)
@@ -259,10 +332,15 @@ public class Simulation : IDisposable
         return target == Target.Shelf;
     }
 
-    private static bool IsFull(string target, Dictionary<string, int> storageLimits, List<Action> actions)
+
+    private static List<(PickableOrder Order, List<Action> Actions)> EntriesForForeignOrdersOnTarget(
+    List<KeyValuePair<string, (PickableOrder Order, List<Action> Actions)>> kvpsWithOrdersOnTarget)
     {
-        // checking if == is actually sufficient
-        return storageLimits[target] <= actions.Count(x => x.Target == target && x.ActionType == ActionType.Place);
+        var result = kvpsWithOrdersOnTarget
+        .Where(kvp => kvp.Value.Actions.Last().Target != ToTarget(kvp.Value.Order.Temp))
+        .Select(kvp => kvp.Value)
+        .ToList();
+        return result;
     }
 
     private static bool IsFull_Flawed(
@@ -270,12 +348,12 @@ public class Simulation : IDisposable
         Dictionary<string, int> storageLimits,
         Dictionary<string, (PickableOrder Order, List<Action> Actions)> _actionRepo)
     {
-        var entriesWithOrdersOnTarget = EntriesWithOrdersOnTarget_Flawed(_actionRepo, target).ToList();
+        var entriesWithOrdersOnTarget = KvpsWithOrdersOnTarget(_actionRepo, target).ToList();
         // checking if == is actually sufficient
         return storageLimits[target] <= entriesWithOrdersOnTarget.Count;
     }
 
-    private static IEnumerable<KeyValuePair<string, (PickableOrder Order, List<Action> Actions)>> EntriesWithOrdersOnTarget_Flawed(
+    private static List<KeyValuePair<string, (PickableOrder Order, List<Action> Actions)>> KvpsWithOrdersOnTarget(
     Dictionary<string, (PickableOrder Order, List<Action> Actions)> _actionRepo,
     string target)
     {
--- test/Challenge.Tests/SimulateTests.cs
+++ test/Challenge.Tests/SimulateTests.cs
@@ -26,7 +26,7 @@ public class SimulateTests : IDisposable
             { Target.Shelf, 3 },
             { Target.Heater, 3 }};
         _defaultConfig = new Simulation.Config(500_000, 6_000_000, 8_000_000, storageLimits);
-        // _cts = new CancellationTokenSource(5_000);
+        _cts = new CancellationTokenSource(5_000);
         _cts = new CancellationTokenSource(5000_000);
 
     }
@@ -298,41 +298,44 @@ public class SimulateTests : IDisposable
         Assert.Equal(Target.Shelf, actions.First(x => x.Id == "6").Target);
     }
 
-    [Fact()]
-    public async Task Moves_NonShelfOrderToNonShelfStorage_WhenShelfOrderArrives()
+    [Theory()]
+    [InlineData(Temperature.Cold, Temperature.Hot)]
+    [InlineData(Temperature.Hot, Temperature.Cold)]
+
+    public async Task Moves_NonShelfOrderToNonShelfStorage_WhenShelfOrderArrives(string testTemp, string oppositeTemp)
     {
         // Arrange
-        var testTarget = Target.Cooler;
-        var oppositeTarget = Target.Heater;
-
         Dictionary<string, int> storageLimits = new() {
-            { Target.Cooler, 1 },
-            { Target.Shelf, 1 },
-            { Target.Heater, 999 }};
-        Simulation.Config expireInFiveOrdersConfig = new(1_000_000, 5_000_000, 5_000_000, storageLimits);
+            { Simulation.ToTarget(testTemp), 1 },
+            { Target.Shelf, 2 },
+            { Simulation.ToTarget(oppositeTemp), 999 }};
+        Simulation.Config expireInFiveOrdersConfig = new(1_000_000, 4_800_000, 4_900_000, storageLimits);
 
-        Order orderToExpireBeforeShelfArrives = new("x1", "Banana", Temperature.Cold, 20, 50);
+        Order orderToPickupBeforeShelfArrives = new("x1", "Banana", testTemp, 20, 50);
         List<Order> timeFillingOrders1 = Enumerable
-            .Range(2, 3)
-            .Select(x => new Order("f1" + x.ToString(), "Banana", Temperature.Hot, 20, 60))
+            .Range(1, 2)
+            .Select(x => new Order("o1." + x.ToString(), "Banana", oppositeTemp, 20, 60))
             .ToList();
-        Order orderForcedToShelf = new("x2", "Banana", Temperature.Cold, 20, 50);
+        Order orderForShelf = new("s1", "Banana", Temperature.Room, 20, 50);
+        Order orderForcedToShelf = new("x2", "Banana", testTemp, 20, 50);
         List<Order> timeFillingOrders2 = Enumerable
-            .Range(2, 3)
-            .Select(x => new Order("f2" + x.ToString(), "Banana", Temperature.Hot, 20, 60))
+            .Range(1, 2)
+            .Select(x => new Order("o2." + x.ToString(), "Banana", oppositeTemp, 20, 60))
             .ToList();
-        Order shelfOrder = new("x3", "Banana", Temperature.Room, 20, 50);
+        Order shelfOrder = new("s2", "Banana", Temperature.Room, 20, 50);
 
-        List<Order> orders = [orderToExpireBeforeShelfArrives, .. timeFillingOrders1, orderForcedToShelf, .. timeFillingOrders2, shelfOrder];
+        List<Order> orders = [orderToPickupBeforeShelfArrives, .. timeFillingOrders1, orderForShelf, orderForcedToShelf, .. timeFillingOrders2, shelfOrder];
 
         // Act
         var actions = await SimulateToTheEnd(expireInFiveOrdersConfig, orders, _cts.Token);
 
         // Assert
-        var moveActions = actions.Where(x => x.Id == "x2" && x.ActionType == ActionType.Move).ToList();
-        Assert.True(moveActions.Count == 1, $"Expected single {ActionType.Move} action for {"x2"} order, but found {moveActions.Count}");
-        Assert.Equal(Target.Cooler, moveActions.Single().Target);
+        var moveActions = actions.Where(x => x.Id == orderForcedToShelf.Id && x.ActionType == ActionType.Move).ToList();
+        Assert.True(moveActions.Count == 1, $"Expected single {ActionType.Move} action for {orderForcedToShelf.Id} order, but found {moveActions.Count}");
+        Assert.Equal(Simulation.ToTarget(testTemp), moveActions.Single().Target);
     }
+
+
 
     private static string ActionsForErrorMessage(List<Action> actions)
     {
